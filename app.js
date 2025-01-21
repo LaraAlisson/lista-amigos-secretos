a617966344f0977cafb27b860604c361e8d497ef
Alisson Ramao Lara <alisson.lara@videplast.com.br>
2025-01-21 09:46:59 -0300

    Criação da função para comparar se o nome já existe na lista de amigos

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação.
 //  Aqui você deverá desenvolver a lógica para resolver o problema.
-let nomesAmigos = [];
+let listaNomesAmigos = [];
 
 function adicionarAmigo() {
 
@@ -11,19 +11,24 @@ function adicionarAmigo() {
         //Retira os espaços do inicio e do fim da variavel.
         let nome = inputNome.value.trim();
 
-        
         //Compara se a string não está vazia.
         if (nome === '') {
             alert('Por favor, preencha o campo nome para adicionar um item na lista.');
             return;
         }
 
+        //verefica se o nome já existe na lista
+        if(listaNomesAmigos.includes(nome)){
+            alert('Este nome já existe na sua lista de amigos.');
+            return;
+        
+        }
         //Adiciona o elemento no array
-        nomesAmigos.push(nome);
+        listaNomesAmigos.push(nome);
 
         //Limpa o campo input 
         inputNome.value = '';
-        //console.log(nomesAmigos);
+        console.log(listaNomesAmigos);
     } else {
         //console.error('Elemento input não encontrado');
     }
