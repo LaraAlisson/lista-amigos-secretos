182aa5a13fb6ccd3146fb955855e9af34aa36be2
aAlissonLara <alissonlarra@gmail.com>
2025-01-21 23:30:06 -0300

    Criado a função para atualizar a lista de amigos e alterado nome da lista, e alterado o tipo de algumas variaveis de let para const

--- app.js
+++ app.js
@@ -1,36 +1,57 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação.
 //  Aqui você deverá desenvolver a lógica para resolver o problema.
-let listaNomesAmigos = [];
+const listaDeAmigos = [];
 
 function adicionarAmigo() {
 
     //recebe o valor do campo input.
     const inputNome = document.querySelector('#amigo');
 
-    if (inputNome) {
-        //Retira os espaços do inicio e do fim da variavel.
-        let nome = inputNome.value.trim();
-
-        //Compara se a string não está vazia.
-        if (nome === '') {
-            alert('Por favor, preencha o campo nome para adicionar um item na lista.');
-            return;
-        }
-
-        //verefica se o nome já existe na lista
-        if(listaNomesAmigos.includes(nome)){
-            alert('Este nome já existe na sua lista de amigos.');
-            return;
-        
-        }
-        //Adiciona o elemento no array
-        listaNomesAmigos.push(nome);
-
-        //Limpa o campo input 
-        inputNome.value = '';
-        console.log(listaNomesAmigos);
-    } else {
-        //console.error('Elemento input não encontrado');
+    //Verifica se o elemento de input foi encontrado.
+    if (!inputNome) {
+        console.error('Elemento input não encotrado');
+        return;
     }
+    //Retira os espaços do inicio e do fim da variavel.
+    let nome = inputNome.value.trim();
+
+    //Verefica  se o nome está vazio.
+    if (nome === '') {
+        alert('Por favor, preencha o campo nome para adicionar um item na lista.');
+        inputNome.value = '';
+        return;
+    }
+
+    //verefica se o nome já existe na lista
+    if (listaDeAmigos.includes(nome)) {
+        alert('Este nome já existe na sua lista de amigos.');
+        inputNome.value = '';
+        return;
+    }
+
+    //Adiciona o elemento no array
+    listaDeAmigos.push(nome);
+
+    //Atualiza a lista exibida na tela
+    renderizarListaAmigos();
+
+    //Limpa o campo input 
+    inputNome.value = '';
+    console.log(listaDeAmigos);
 }
 
+
+function renderizarListaAmigos() {
+    const listaAmigosElement = document.getElementById('listaAmigos');
+
+    //limpa a lista antes de atualizar
+    listaAmigosElement.innerHTML = '';
+
+    //Adiciona cada amigo à lista
+    listaDeAmigos.forEach(nome => {
+        const li = document.createElement('li');
+        li.textContent = nome;
+        document.getElementById('listaAmigos').appendChild(li);
+    });
+    
+}
